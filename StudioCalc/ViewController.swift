a99af95f1e8b591e7e9fab9d702c0a6055d6fe28
dougolson <mrcolson@gmail.com>
2016-10-02 00:27:47 -0500

    Lots of changes, experiments to make screen flash click work. About to clean out failed code

--- StudioCalc/ViewController.swift
+++ StudioCalc/ViewController.swift
@@ -14,11 +14,16 @@ class ViewController: UIViewController {
     var timeArr = [Double]()
     var count = 0
     var timeDiff = 0.0
+    var timeMean = 0.0
     var bpm = 0.0
     var noteVals = [1,2,4,8,16,32]
+    var clickOn = true
     let noteDefaultColor = UIColor(red: 176/255, green: 228/255, blue: 145/255, alpha: 0.8)
     let noteClickedColor = UIColor(red: 176/255, green: 228/255, blue: 145/255, alpha: 0.0)
+    let bgColor = UIColor(red: 152/255, green: 204/255, blue: 191/255, alpha: 1.0)
+    let bgColorDark = UIColor(red: 102/255, green: 153/255, blue: 153/255, alpha: 1.0)
     
+    var timer = Timer()
 
     @IBOutlet weak var bpmResult: UILabel!
     @IBOutlet weak var delayResult: UILabel!
@@ -27,13 +32,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        switch count {
+//        case 0:
+//            break
+//        case 1:
+//            break
+//        default:
+//            self.timer = Timer.scheduledTimer(timeInterval: timeMean, target: self,  selector: #selector(ViewController.Tick), userInfo: nil, repeats: true)
+//        }
     }
+        // Do any additional setup after loading the view, typically from a nib.
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    // reset
     
     func resetState() {
         time = 0.0
@@ -44,41 +62,45 @@ class ViewController: UIViewController {
         bpmResult.text = "Tempo"
         delayResult.text = "Delay"
         hzResult.text = "Hz"
-        for var i in noteVals {
+        for i in noteVals {
             if let button = self.view.viewWithTag(i) as? UIButton {
                 button.backgroundColor = noteDefaultColor
             }
         }
+//        self.timer.invalidate()
+        view.layer.removeAllAnimations()
+
     }
     
+    // Tap Tampo
     @IBAction func buttonTapped(_ sender: AnyObject) {
         
         time = CACurrentMediaTime() // get device time
         timeArr.append(time) // array of time values
-        print(count)
         switch count{
-            case 0:
+            case 0...1:
                 timeDiff = 0.0
-            case 1:
-                timeDiff = 0.0
+                bpmResult.text = "..."
             default:
                 timeDiff = timeArr[timeArr.count - 1] - timeArr[0]
+                timeMean = timeDiff / Double(count)
                 bpm = round(10 * 60 * Double(count) / timeDiff) / 10
+                // reset after 2 seconds
                 if (timeArr[timeArr.count - 1] - timeArr[timeArr.count - 2]) > 2 {
                     resetState()
                     return
                 }
+                bpmResult.text = "\(bpm) B.P.M"
+                screenFlashTimer()
             }
-        
-        bpmResult.text = "\(bpm) B.P.M"
-        
         count += 1
-
     }
     
-
+    // Note Values
+    
     @IBAction func noteValClicked(_ sender: UIButton) {
         let delayTime = round(240000.0 / (Double(sender.tag) * bpm))/1000.0
+//        currentNoteValue = delayTime
         let frequency = round(100 * bpm * Double(sender.tag) / (4 * 60.0)) / 100
         if bpm > 0 {
             delayResult.text = "\(delayTime) sec"
@@ -88,16 +110,64 @@ class ViewController: UIViewController {
     }
     
     func noteButtonUpdate (tag: Int) {
-        for var i in noteVals {
+        for i in noteVals {
             if let button = self.view.viewWithTag(i) as? UIButton {
                 button.backgroundColor = noteDefaultColor
-                }
-            }
-            if let button = self.view.viewWithTag(tag) as? UIButton {
-                button.backgroundColor = noteClickedColor
             }
         }
+        if let button = self.view.viewWithTag(tag) as? UIButton {
+            button.backgroundColor = noteClickedColor
+        }
+    }
+    
+    
+    
+    
+    // Click start/stop
+    
+//    @IBAction func clickStart(_ sender: UIButton) {
+//        if timeMean > 0.1 {
+//            switch clickOn {
+//            case true:
+//                self.timer = Timer.scheduledTimer(timeInterval: timeMean / 2.0, target: self,  selector: #selector(ViewController.screenFlashClick), userInfo: nil, repeats: true)
+//                clickOn = false
+//            case false:
+//                self.timer.invalidate()
+//                clickOn = true
+//            }
+//        }
+//    }
+    
+    
+    func screenFlashTimer() {
+        if timeMean > 0.1 {
+            screenFlashClick()
+//            self.timer = Timer.scheduledTimer(timeInterval: timeMean / 2.0, target: self,  selector: #selector(ViewController.screenFlashClick), userInfo: nil, repeats: true)
+        }
+    }
+    
+    var foo = 1
+    
+    func screenFlashClick() {
+        view.layer.removeAllAnimations()
+        UIView.animate(withDuration: timeMean, delay: 0, options: [.allowUserInteraction, .curveEaseInOut, .repeat], animations: {
+//            self.view.alpha = 0.6
+            self.view.backgroundColor = self.bgColorDark
+        }) { (true) in
+//            self.view.alpha = 1.0
+            self.view.backgroundColor = self.bgColor
+        }
+
+    }
+    
+
+    
+    
+
+    
 }
+    
+    
 
 
 
